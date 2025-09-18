7db6d2ecc49a711ab299ca7809d0edde9d598cd8
ajanb <ajanbalaganesh@cmail.carleton.ca>
2025-09-18 15:44:52 -0400

    added a few buddy operations.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,7 +4,10 @@ public class AddressBook {
     private ArrayList<BuddyInfo> buddies;
 
     public static void main(String[] args){
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", 613);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
     public AddressBook() {
         buddies = new ArrayList<>();
